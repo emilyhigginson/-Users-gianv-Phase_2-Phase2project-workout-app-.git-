051957671c73186a7275184aef48c8fe86d5bebd
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-02-14 11:05:41 -0500

    Updated DBJson

---
+++ src/DisplayCase.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Display from './Display'
+
+
+
+function DisplayCase(props) {
+  console.log(props);
+    return (
+    <div><Display /></div>
+  )
+}
+
+export default DisplayCase
