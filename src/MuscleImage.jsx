f861ed5a01c8b346f7a8fd9513aec94c9b6aaed7
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-02-15 11:51:05 -0500

    Working Pictures

--- src/MuscleImage.jsx
+++ src/MuscleImage.jsx
@@ -1,34 +1,35 @@
 import React from 'react'
-import abs from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/abs.png'
-import bicep from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/MuscleImages/bicep.png'
-import chest from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/MuscleImages/chest.png'
-import glute from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/MuscleImages/glute.png'
-import quad from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/MuscleImages/quad.png'
-import shoulder from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/MuscleImages/shoulder.png'
-import tricep from '/Users/emilyhigginson/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/MuscleImages/tricep.png'
+import abs from './MuscleImages/abs.png'
+import bicep from './MuscleImages/bicep.png'
+import chest from './MuscleImages/chest.png'
+import glute from './MuscleImages/glute.png'
+import quad from './MuscleImages/quad.png'
+import shoulder from './MuscleImages/shoulder.png'
+import tricep from './MuscleImages/tricep.png'
+
 function MuscleImage({area}) {
 
     let image = "";
     if (area =="Abs") {
-        image = "/static/media/abs.9b455d74d33c603c88a0.png"
+        image = abs
     }
     else if (area =="Biceps") {
-        image = "/static/media/bicep.69385744d92834012fe9.png";
+        image = bicep;
     }  
     else if (area== "Chest") {
-      image = "/static/media/chest.ef7978dd618cc736ee8b.png";
+      image = chest;
     }
     else if (area == "Glutes") {
-        image = "/static/media/glute.38768b1c6a7eeb8aaa0b.png";
+        image = glute;
     }
     else if (area == "Quads"){
-        image = "/static/media/quad.f6e651efe91a50ed2738.png";
+        image = quad;
     }
     else if (area == "Shoulders"){
-        image = "/static/media/shoulder.78ca1c3389977b53cee8.png";
+        image = shoulder;
     }
     else if (area == "Triceps"){
-        image = "/static/media/tricep.4990e65c47aab1cc1839.png";
+        image = tricep;
     }
 
   return (
