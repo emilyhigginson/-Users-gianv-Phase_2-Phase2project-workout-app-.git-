40ee405c0542b632feaefad3e12d1890faabfb37
gianv322 <90941304+gianv322@users.noreply.github.com>
2022-02-15 18:00:29 -0500

    more styling

--- src/WorkoutCard.jsx
+++ src/WorkoutCard.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import MuscleImage from "./MuscleImage";
+import x from "/Users/gianv/Phase_2/Phase2project/-Users-gianv-Phase_2-Phase2project-workout-app-.git-/src/fonts/highlight_off_black_24dp.svg";
 
 function WorkoutCard({ wo, onAddWorkout }) {
   const [isAdded, setIsAdded] = useState(false)
@@ -26,15 +27,16 @@ function WorkoutCard({ wo, onAddWorkout }) {
   return (
     <div className="card" >
       <div className="modalCard" style={{display:style}}>
-        <button className="closeModal" onClick={toggleCard}>x</button>
+        <button className="closeModal" onClick={toggleCard}><img src={x}/></button>
         <h2>How to: {name} </h2>
         <p>{instructions}</p>
       </div>
-      <p className="workoutName" onClick={toggleCard} >{name}</p>
-      <p className="muscleGroup">{area}</p>
-      <MuscleImage area={area} />
-      <p>Difficulty: {difficulty}/5</p>
-
+      <div className="workoutinfo" onClick={toggleCard}>
+        <p className="workoutName">{name}</p>
+        <p className="muscleGroup">{area}</p>
+        <MuscleImage area={area} />
+        <p>Difficulty: {difficulty}/5</p>
+      </div>   
       <button onClick={handleAddWorkout}>{isAdded ? " Remove from Workout" : "Add to Workout"}</button>
     </div>
   );
